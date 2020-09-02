bc22503ebcb6a0581133597f78c42c9129ef58fe
Yang An Boada <anniean8908@gmail.com>
2020-09-02 12:18:07 -0700

    Correct function| Yang An Boada

--- Practice-Problems/create-repeating-letters/main.js
+++ Practice-Problems/create-repeating-letters/main.js
@@ -1,8 +1,12 @@
 function createRepeatingLetters(word, numOfRepeats) {
-  while(numOfRepeats){
-    console.log(word);
-    numOfRepeats--;
+  var newWord = '';
+  for (var i = 0; i < word.length; i++){
+    for (var j = 0; j < numOfRepeats; j++){
+      newWord += word[i];
+    }
   }
+  return newWord;
 }
-createRepeatingLetters("hello", 3);
-createRepeatingLetters("I love coding", 5);
+
+console.log(createRepeatingLetters("hello", 3));
+console.log(createRepeatingLetters("nice", 5));

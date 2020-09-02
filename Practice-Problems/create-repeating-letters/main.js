ab49c99bc1c86b15cd1a2551ac8d721fd7718ebb
Yang An Boada <anniean8908@gmail.com>
2020-09-02 10:52:19 -0700

    Create Repeating Letters | Yang An Boada

---
+++ Practice-Problems/create-repeating-letters/main.js
@@ -0,0 +1,8 @@
+function createRepeatingLetters(word, numOfRepeats) {
+  while(numOfRepeats){
+    console.log(word);
+    numOfRepeats--;
+  }
+}
+createRepeatingLetters("hello", 3);
+createRepeatingLetters("I love coding", 5);
